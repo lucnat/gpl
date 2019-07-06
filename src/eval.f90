9e50498797f7e000bb97da1c267dc6df5246e8d9
Luca Naterop <luca.naterop@bluewin.ch>
2019-07-06 18:24:01 +0200

    minor

--- src/eval.f90
+++ src/eval.f90
@@ -7,27 +7,15 @@ PROGRAM eval
 
   implicit none
   
-  integer :: i
   complex(kind=prec) :: res 
-  ! complex(kind=prec) :: a(3), s(2)
-  ! complex(kind=prec) :: alpha(product((/(i,i=1,size(a)+size(s))/))/  & 
-  !     (product((/(i,i=1,size(a))/))*product((/(i,i=1,size(s))/))), & 
-  !     size(a) + size(s))
 
   call parse_cmd_args()
 
-  ! a = cmplx((/1,2,1/))
-  ! s = cmplx((/4.0,42e50/))
-
-  ! alpha = shuffle_product(a,s)
-  ! call print_logical_matrix(alpha == 42e50)
-  ! print*, find_first_true(alpha(6,:) == 42e50)
-
-  ! res = GPL([2,1,3,4]) ! here's an example where we need to shuffle away from last place
-  ! print*, res
-  
-  res = pending_integral(cmplx([1,3]),2,cmplx([2,4]))
+  res = GPL([0.0, 3.3333333333333335, 1.0, 3.3333333333333335, 1.0])
   print*, res
+  
+  ! res = pending_integral(cmplx([1,3]),2,cmplx([2,4]))
+  ! print*, res
 
 END PROGRAM eval
 
